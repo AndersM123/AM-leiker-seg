9cbda8e9c8b8f4ecb3ce2fff121537ee96f7ab93
AndersM123 <69676974+AndersM123@users.noreply.github.com>
2020-10-01 12:36:10 +0200

    øvingsoppgaver

---
+++ Øvingsoppgaver/src/pakke/Oppg_uke_38.java
@@ -0,0 +1,17 @@
+package pakke;
+
+public class Oppg_uke_38 {
+//a)
+	public static void main(String[] args) {
+System.out.print("x i grader:           " + "x i radianer  ");
+		for (int i = 0; i <= 180; i = i + 15) {
+			double r = (Math.PI * i) / 180;
+		
+		       
+		
+		System.out.println(i + "  " + r);
+		}
+		
+	}
+
+}
