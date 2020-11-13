d2c5aa1fe5baba8bb501f475abce2cd43d725be3
AndersM123 <69676974+AndersM123@users.noreply.github.com>
2020-11-13 13:46:33 +0100

    oppgave 2 v19

---
+++ EksamenssettV19/src/oppgave2/Bilde.java
@@ -0,0 +1,16 @@
+package oppgave2;
+
+public class Bilde extends Innlegg {
+	private String url;
+	
+	
+	public Bilde(String bruker, String dato, String url) {
+		super(bruker, dato);
+		this.url=url;
+	}
+	@Override
+	public void skrivUt() {
+		super.skrivUt();
+		System.out.println("Url: " + url);
+	}
+}
